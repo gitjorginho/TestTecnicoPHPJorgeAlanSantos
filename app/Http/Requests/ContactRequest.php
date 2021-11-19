e9868d89f32d3be3b9bd9c2305e0ef35e26f19d8
Jorge <jorgeallan@msn.com>
2021-11-19 16:08:57 -0300

    traducao de validacao

--- app/Http/Requests/ContactRequest.php
+++ app/Http/Requests/ContactRequest.php
@@ -26,9 +26,26 @@ class ContactRequest extends FormRequest
         return [
             "name" => "required|max:70",
             "email" => "required|email" ,
-            "telephone" =>"required",
+            "telephone" =>['required','regex:/^\(\d{2}\)\d{4,5}-\d{4}$/'],
             "message" => "required",
             "file"=> "required|mimes:pdf,doc,docx,odt,txt|max:500",
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required'=>'Nome é um campo obrigatório',
+            'name.max'=>'O campo nome deve ter menos de 70 caracteres',
+            'email.required'=>'Email é um campo obrigatório',
+            'email.email'=>'Email inválido',
+            'telephone.required'=>'Telefone é um campo obrigatório',
+            'telephone.regex'=>'Telefone inválido',
+            'message.required'=>'Menssagem é um campo obrigatório',
+            'file.required'=>'Nanhum arquivo foi anexado',
+            'file.mimes'=>'O arquivo anexado deve ter a extensão: pdf,doc,docx,odt ou txt ',
+            'file.max'=>'O Arquivo deve ter o tamanho maximo de 500k',
+
         ];
     }
 }
