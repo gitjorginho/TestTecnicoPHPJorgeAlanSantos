3a173ec87d82fc60e985fb55d0edc59134e20f7d
Jorge <jorgeallan@msn.com>
2021-11-19 18:31:07 -0300

    test unitario

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,8 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Contact;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
 
 class ExampleTest extends TestCase
 {
@@ -12,10 +15,99 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testGetAll()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response = $this->get('/api/contact');
+        $response->assertOk();
+        $response->assertJsonStructure([
+            "*"=>[
+                "id",
+                "name",
+                "email",
+                "telephone",
+                "message",
+                "file"
+            ]
+        ]);
     }
+   
+
+
+    public function testStore(){
+        
+        $request = [
+            "name" => "Jorge",
+            "email" => "jorgeallan@msn.com",
+            "telephone" => "(71)98506-7620",
+            "message" => "Estou testando um software",
+            "file" => new UploadedFile(resource_path('doc/curriculum.pdf'), 'test.pdf', null, null, true),
+        ];
+        DB::beginTransaction();     
+        $response = $this->post('/api/contact',$request);
+        $response->assertOk();
+        $response->assertJsonStructure([
+            "id",
+            "name",
+            "email",
+            "telephone",
+            "message",
+            "file"
+        ]);
+        unset($request['file']);
+        $response->assertJsonFragment($request);
+        DB::rollBack();     
+    }
+
+    public function testUpdate(){
+        
+        $contacts = Contact::limit(100)->get();
+        $contact = array_rand($contacts->all(), 1);
+        $contact_id = $contacts[$contact]->id;
+        $request = [
+            "_method" => "PUT",
+            "name" => "Jorge Alan",
+            "email" => "jorgeallan@msn.com",
+            "telephone" => "(71)98506-7620",
+            "message" => "Estou testando um aplicativo",
+            "file" => new UploadedFile(resource_path('doc/curriculum.pdf'), 'test.pdf', null, null, true),
+        ];
+        DB::beginTransaction();     
+        $response = $this->withHeader('Content-Type','multipart/form-data')->post('/api/contact/'.$contact_id,$request);
+        $response->assertOk();
+        
+        $response->assertJsonStructure([
+            "id",
+            "name",
+            "email",
+            "telephone",
+            "message",
+            "file"
+        ]);
+        unset($request['file']);
+        unset($request['_method']);
+        $response->assertJsonFragment($request);
+        DB::rollBack();     
+    }
+
+    public function testDestroy(){
+               
+        $contacts = Contact::limit(100)->get();
+        $contact = array_rand($contacts->all(), 1);
+        $contact_id = $contacts[$contact]->id;
+        
+        DB::beginTransaction();     
+        $response = $this->delete('/api/contact/'.$contact);
+        $response->assertOk();
+        $response->assertJsonStructure([
+            "id",
+            "name",
+            "email",
+            "telephone",
+            "message",
+            "file"
+        ]);
+        
+        DB::rollBack();     
+    }
+
 }
